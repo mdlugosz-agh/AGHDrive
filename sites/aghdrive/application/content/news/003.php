b4007b3dec059a574ff5e90a85a610dbe23f0685
mdlugosz <mdlugosz@gmail.com>
2024-07-16 18:52:11 +0200

    Remove https from remot content link

--- sites/aghdrive/application/content/news/003.php
+++ sites/aghdrive/application/content/news/003.php
@@ -1,8 +1,8 @@
 <?php
 $item = array(
-    'title' => file_get_contents('https://loripsum.net/api/1/short/plaintext'), 
-    'short' => file_get_contents('https://loripsum.net/api/1/short'),
-    'text' =>  file_get_contents('https://loripsum.net/api/3/medium'),
+    'title' => file_get_contents('http://loripsum.net/api/1/short/plaintext'), 
+    'short' => file_get_contents('http://loripsum.net/api/1/short'),
+    'text' =>  file_get_contents('http://loripsum.net/api/3/medium'),
     'date' => '2022-01-02');
 
 return $item;
